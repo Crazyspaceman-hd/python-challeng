0f26d85b616fa46bd6a8a9aed22e8288ee34950a
unknown <hvdeane@gmail.com>
2021-01-10 15:50:28 -0800

    Added average and profit/loss changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,12 @@ import os
 import csv
 months=0
 net=0
+before=0
+change_tote=0
+prof_max=0
+prof_min=0
+
+
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
@@ -14,9 +20,38 @@ with open(csvpath, newline='') as csvfile:
         months=months+1
         # net total profit/loss
         net= net + int(row[1])
+        # average of changes in profit/loss
+        change= int(row[1])-before
+        before=int(row[1])
+        change_tote=change_tote+change
+        # greatest profits date and amount
+        if change > prof_max:
+            prof_max= change
+            date_prof_max= row[0]
+        if change < prof_min:
+            prof_min= change
+            date_prof_min= row[0]
+        
+
+       
+    change_avg=round(change_tote/months, 2)
+    
+print("Financial Analysis")
+print("-------------------------------")
+print("Total Months: " + str(months))
+print("Total: $"+ str(net))
+print("Average Change: $"+ str(change_avg))
+print("Greatest Increase in Profits: "+ date_prof_max +" $"+str(prof_max))
+print("Greatest Decrease in Profits: "+ date_prof_min +" $"+str(prof_min))
+    
+
         
 
 
-# average of changes in profit/loss
+
+        
+
+
+
 # greatest profits date and amount
 # greatest loss date and amount
