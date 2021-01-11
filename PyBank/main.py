42f72752a9e90c2ea10fb94638292879e16781af
unknown <hvdeane@gmail.com>
2021-01-10 21:52:48 -0800

    Working code that performs all requested functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,18 +10,21 @@ prof_min=0
 
 
 
-csvpath = os.path.join("Resources", "budget_data.csv")
+budget = os.path.join("Resources", "budget_data.csv")
 
-with open(csvpath, newline='') as csvfile:
+with open(budget, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
     for row in csvreader:
+        if before == 0:
+            before=int(row[1])
+        print (before)
         # total number of months
         months=months+1
         # net total profit/loss
         net= net + int(row[1])
         # average of changes in profit/loss
-        change= int(row[1])-before        
+        change= int(row[1])-before       
         before=int(row[1])
         change_tote=change_tote+change
         # greatest profits date and amount
@@ -32,15 +35,24 @@ with open(csvpath, newline='') as csvfile:
         if change < prof_min:
             prof_min= change
             date_prof_min= row[0]
-        
+        print (change)
 
        
-    change_avg=round(change_tote/months, 2)
+    change_avg=round(change_tote/(months-1), 2)
     
 print("Financial Analysis")
 print("-------------------------------")
 print("Total Months: " + str(months))
 print("Total: $"+ str(net))
 print("Average Change: $"+ str(change_avg))
-print("Greatest Increase in Profits: "+ date_prof_max +" $"+str(prof_max))
-print("Greatest Decrease in Profits: "+ date_prof_min +" $"+str(prof_min))
+print("Greatest Increase in Profits: "+ date_prof_max +" ($"+str(prof_max) + ")")
+print("Greatest Decrease in Profits: "+ date_prof_min +" ($"+str(prof_min) + ")")
+
+with open('Financial_Analysis.txt', 'w') as f:
+    print('Financial Analysis', file=f)
+    print("-------------------------------", file=f)
+    print("Total Months: " + str(months), file=f)
+    print("Total: $"+ str(net), file=f)
+    print("Average Change: $"+ str(change_avg), file=f)
+    print("Greatest Increase in Profits: "+ date_prof_max +" ($"+str(prof_max) + ")", file=f)
+    print("Greatest Decrease in Profits: "+ date_prof_min +" ($"+str(prof_min) + ")", file=f)
