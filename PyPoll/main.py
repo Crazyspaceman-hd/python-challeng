a5928400be22e537c4ce78e9c1714c55dc84d4c9
unknown <hvdeane@gmail.com>
2021-01-11 22:05:09 -0800

    Added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,16 @@
 #Import .csv
 import os
 import csv
+#declare lists and dictionaries
 votes = []
 final_votes = []
 percentage = []
 results ={}
 
+#create path to CSV file
 csvpath = os.path.join("Resources", "election_data.csv")
 
-
-#percentage of votes for each candidate
-
-#winner of the election
-#loop through list seperating votes into list
+#loop through CSV seperating votes into list
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
@@ -28,24 +26,28 @@ for name in candidates:
     final_votes.append (votes.count(name))
     results[name] = votes.count(name)
 
-
+#print summary
 print("Election Results")
 print("---------------------------")
 print("Total Votes: ", votes_total)
 print("---------------------------")
+#loop through candidate:votes dictionary printing results
 for p, v in results.items():
-    print(f'{p}:  {"{:.3f}".format((v/votes_total)*100)}% ({v}) ')
+    #print candidates name, set format and calculate percent of vote received, print number of candidate votes
+    print(f'{p}:  {"{:.3%}".format(v/votes_total)} ({v}) ')
 print("---------------------------")
+#determine winner from results dictionary and print
 print (f'Winner:  {max(results, key=results.get)}')
 print("---------------------------")
 
+#send summary to text file
 with open('./Analysis/Election_results.txt', 'w') as f:
-    print("Election Results", file=f)
-    print("---------------------------", file=f)
-    print("Total Votes: ", votes_total, file=f)
-    print("---------------------------", file=f)
-    for p, v in results.items():
-       print(f'{p}:  {"{:.3f}".format((v/votes_total)*100)}% ({v}) ', file=f)
-    print("---------------------------", file=f)
-    print (f'Winner:  {max(results, key=results.get)}', file=f)
-    print("---------------------------", file=f)
+     print("Election Results", file=f)
+     print("---------------------------", file=f)
+     print("Total Votes: ", votes_total, file=f)
+     print("---------------------------", file=f)
+     for p, v in results.items():
+        print(f'{p}:  {"{:.3%}".format(v/votes_total)} ({v}) ', file=f)
+     print("---------------------------", file=f)
+     print (f'Winner:  {max(results, key=results.get)}', file=f)
+     print("---------------------------", file=f)
